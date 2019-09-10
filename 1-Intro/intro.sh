130dbc80fcb949f7c8c9e5003486ec94136ef132
mebaysan <menesbaysan@gmail.com>
2019-09-11 00:55:10 +0300

    My first bash scripts Intro to the Bash Language

---
+++ 1-Intro/intro.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+# ilk satır bunun bir bash olduğunu belirtiyor, sonra verilen dizine git dedik ve ./pycharm.shi çalıştır dedik. Bourne Again Shell -> BASH
+<<Yorum1
+	bu şekilde yorum satırları da yapabiliriz. Çoklu yorum satırlarını sanki bir html body extend eder gibi block block yazarız.
+	eğer root kullanıcısı değilsek yazdığımız bir bash scripti direk çalıştıramayız onu execute etmeliyiz.
+	chmod +x myShell.sh -> bu şekilde bash scripti çalışabilir hale getiriyoruz
+
+Yorum1
+# ayrıca bu '#' yorum satırıdır
+cd /root/Desktop/pycharm-2019.2.1/bin/
+./pycharm.sh
---
+++ 1-Intro/intro2.sh
@@ -0,0 +1,4 @@
+#! /bin/bash
+#standart bir hello world yapalım :)
+
+echo "Naber Nasılsın, Enes Baysan Bash'e de zıpladı :)"
